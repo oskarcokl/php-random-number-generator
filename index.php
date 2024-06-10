f9aa265e84edcf124b94ceb82768aa48365028ae
coskar <oskar.cokl@iprom.si>
2024-06-10 15:00:24 +0200

    feat: Reading number of dice and type of dice.

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>PHP Test</title>
-    </head>
-    <body>
-        <?php echo '<p>Hello World</p>'; ?>
-    </body>
+
+<head>
+    <title>PHP Test</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label for="numberDice">Number of dice to roll:</label>
+        <input name="numberDice" id="numberDice" type="number">
+
+        <label for="diceType">Your age:</label>
+        <select name="diceType" id="diceType">
+            <option value="4">d4</option>
+            <option value="6">d6</option>
+            <option value="10">d10</option>
+            <option value="12">d12</option>
+            <option value="20">d20</option>
+        </select>
+
+        <button type="submit">Roll</button>
+    </form>
+    <?php
+
+
+
+
+
+
+        if (isset($_POST['numberDice']) && isset($_POST['diceType'])) {
+            echo $_POST['numberDice'] . '<br>';
+            echo $_POST['diceType'];
+        }
+    ?>
+</body>
+
 </html>
